d8ab8e0d7b720803a74de50a2b678fe8b586d1a6
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-03-11 20:04:50 -0700

    Create src.py

---
+++ src.py
@@ -0,0 +1,15 @@
+#defining and creating the function. 
+def my_function(x):  
+
+
+ #return the string
+
+ return x[::-1] #<< slice the string
+
+
+#Call the function
+mytxt = my_function("I wonder how this text looks like backwards")
+
+
+#Print the result
+print(mytxt)
